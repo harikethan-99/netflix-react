81a751db69c1e54514a7b18d1b6c3124f28f801d
Hari Kethan <harikethan1999@gmail.com>
2021-01-30 09:50:26 +0530

    Second Commit

---
+++ src/Movies.js
@@ -0,0 +1,41 @@
+export const arr =[
+    {
+      image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaUD92whdwqVnEPh5fMHOBL5vext3SdEnKXR03ZDZ7on7yhm0es-61Q_70kKhas_PtfHMDD49bJJ4F3w015XiAFW7H9cDUzEDl8TFSKPAHWhImqoatBUPOMxuj2N.jpg?r=1f7",
+      name:"Extraction"
+    },{
+      image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABa1nyBEi4gqCXXYJXObKWDwHT3HARnPQTPIi1G0IoXNApiXdldb6HG-M6sAaT4iSEE9L-SljqzTf0jlJl6WRfLvCRYA.jpg?r=845",
+      name:"Aquaman"
+    },{
+      image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ3zNNfnKrKSwjlTOWsRp4FOUkcCpOhQ_I1V6UwpaKbY76dTJ633LfFf-A8AUi0bdL-w8b_-r2MVPyFWKQIqjLqRCQQ.jpg?r=052",
+      name:"Godzilla"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVq1Ck17Qln2Qls2Jef8AZKXw0s48z1pdLcYi0FQfkevO8xka6lZGAvXdxC6HzpoR7ut5-kC24a-fkwPpGDnDzNhbz8.jpg?r=eab",
+      name:"Vikings"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXxTnIX0G41UviehfvgOJkHYqq6GV3rMWqZhXwY7vqwZDzRZXYVqmhC-rg2nDR3BsB6HljF-IIId-klcADSRgCqUd41n5HDbkMQbu5aYWu-ldCsaKXt-nvscvxdg.jpg?r=4ce",
+      name:"Cobra Kai"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXtmKjGQ71E6xL_WDCIxN7rUVLWGfe-AVDOsh9eg57bG9UIfmxzLES2fmd47vZaLnYj_wbOB_FqzVEFgF-gKi3b_zfE.jpg?r=972",
+      name:"Teen Wolf"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXr99-I9s8cPs-OmzMh327Kr7WpYGIHBNO2yFz5HggMEolpHIGWmHpugXt5j7V14ilbGfDqkGacCDprBQ2CJF0iBPYu899N3nk_CTMciN3eDA0KTNedUl58uh7RQ.jpg?r=285",
+      name:"The marvel's Punisher"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABa-oTyIxZjZX2RV59zIXh0I_NOJB16m7Iky-RTZ9jqavv58JOO4FcsoW_WH_vKdt9Cu8ZIZxDmDqb6FvEk6zj2aMp-GajzsrK9qJcFKdVZ7oNofBA9redABpGuwb.jpg?r=973",
+      name:"Narcos"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABV5xckwBcC7GGH5HRPoLhAzAky28i6c7N24FyBihqlNt4NsEGWbxvwrYFNA8XGGNjKWl7tEWr2VcB36lNXKwbYzNc7RSu06BP0nGvi9wYV4zSOE7IV3AjJs07XBS.jpg?r=c05",
+      name:"Altered Carbon"
+    },
+    {
+      image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT-QkFH4dnRnpA67Nht0GW9V_VW0kx-Zby8uPyRX7gs7mQnpJ3ajIXPBLx9eBM7s6lCT0l1UH_lYJjYcX1VEHRK2GTRvbC69gDgbORaalj0C2LDixegEUZV8DuxX.jpg?r=84f",
+      name:"Titans"
+    }
+  ];
+  
--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
+import {arr} from "./Movies";
 let deschead = "Action & Adventure Series";
 const desc = "Time for some action? We’ve got you covered. Fantasy and adventure, mystery and murder, cops and criminals—there’s enough fun for everyone in these TV shows. Watch now for free";
-const arr =[
-  {
-    image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaUD92whdwqVnEPh5fMHOBL5vext3SdEnKXR03ZDZ7on7yhm0es-61Q_70kKhas_PtfHMDD49bJJ4F3w015XiAFW7H9cDUzEDl8TFSKPAHWhImqoatBUPOMxuj2N.jpg?r=1f7",
-    name:"Extraction"
-  },{
-    image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABa1nyBEi4gqCXXYJXObKWDwHT3HARnPQTPIi1G0IoXNApiXdldb6HG-M6sAaT4iSEE9L-SljqzTf0jlJl6WRfLvCRYA.jpg?r=845",
-    name:"Aquaman"
-  },{
-    image:"https://occ-0-2595-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ3zNNfnKrKSwjlTOWsRp4FOUkcCpOhQ_I1V6UwpaKbY76dTJ633LfFf-A8AUi0bdL-w8b_-r2MVPyFWKQIqjLqRCQQ.jpg?r=052",
-    name:"Godzilla"
-  },
-  {
-    image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVq1Ck17Qln2Qls2Jef8AZKXw0s48z1pdLcYi0FQfkevO8xka6lZGAvXdxC6HzpoR7ut5-kC24a-fkwPpGDnDzNhbz8.jpg?r=eab",
-    name:"Vikings"
-  },
-  {
-    image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXxTnIX0G41UviehfvgOJkHYqq6GV3rMWqZhXwY7vqwZDzRZXYVqmhC-rg2nDR3BsB6HljF-IIId-klcADSRgCqUd41n5HDbkMQbu5aYWu-ldCsaKXt-nvscvxdg.jpg?r=4ce",
-    name:"Cobra Kai"
-  },
-  {
-    image:"https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXtmKjGQ71E6xL_WDCIxN7rUVLWGfe-AVDOsh9eg57bG9UIfmxzLES2fmd47vZaLnYj_wbOB_FqzVEFgF-gKi3b_zfE.jpg?r=972",
-    name:"Teen Wolf"
-  }
-];
 function MovieList() {
+  function scrolldiv(){
+    let butt1 = document.getElementById("butt1");
+    butt1.style.visibility = "visible";
+    let item = document.getElementById("div1");
+    item.scrollBy({left:900,top:0,behavior:'smooth'});
+    
+}
+function scrolldiv1(){
+  let item = document.getElementById("div1");
+  item.scrollBy({left:-900,top:0,behavior:'smooth'});
+}
   return(
     <div>
     <nav></nav> 
     <h1>{deschead}</h1> 
     <h3>{desc}</h3>
-    <button type="button" className="butt">&gt;</button> 
-    <article className="innerdiv"> 
+    <button type="button" className="butt" onClick={scrolldiv}>&gt;</button>
+    <button type="button" className="butt1" id="butt1" onClick={scrolldiv1} style={{visibility:"hidden"}}>&lt;</button>
+    <article className="innerdiv" id="div1"> 
     {arr.map((movies) => {
       const {image, name} = movies;
       return (
@@ -51,9 +40,7 @@ const Movie = (props) =>{
   const {image, name} = props.movies;
   return(
     <section>
-    <img
-    src={image}>
-    </img>
+    <img src={image}></img>
     <h4>{name}</h4>
     </section>
   );
